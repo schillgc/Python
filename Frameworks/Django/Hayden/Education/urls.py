0f042585e2307cdb4e10c7bd422259f0710f45ee
Gavin Schilling <schillgc@hotmail.com>
2020-06-03 05:34:27 -0400

    Added Credit_Detail & Credit_List

--- Frameworks/Django/Hayden/Education/urls.py
+++ Frameworks/Django/Hayden/Education/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import CreditDetailView, InstitutionDetailView, InstitutionIndexView
+from .views import CreditDetailView, CreditIndexView, InstitutionDetailView, InstitutionIndexView
 
 app_name = 'Education'
 urlpatterns = [
-    path('', InstitutionIndexView.as_view(), name='index'),
     path('class/<int:pk>/', CreditDetailView.as_view(), name='credit-detail'),
     path('school/<int:pk>/', InstitutionDetailView.as_view(), name='institution-detail'),
+    path('classes', CreditIndexView.as_view()),
+    path('schools', InstitutionIndexView.as_view())
 ]
