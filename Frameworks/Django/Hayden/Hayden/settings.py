541df8f02bd44d1f35b8390f1647081fb9a98f2b
Gavin Schilling <schillgc@hotmail.com>
2020-06-26 20:35:21 -0400

    Add Job app

--- Frameworks/Django/Hayden/Hayden/settings.py
+++ Frameworks/Django/Hayden/Hayden/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'Education.apps.EducationConfig',
+    'Job.apps.JobConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
