8ae3911d712685dd3986ca728a167bdf2da07ea3
Gavin Schilling <schillgc@hotmail.com>
2020-07-30 07:08:48 -0400

    Dried reusable code

--- Frameworks/Django/Hayden/Education/urls.py
+++ Frameworks/Django/Hayden/Education/urls.py
@@ -6,6 +6,6 @@ app_name = 'Education'
 urlpatterns = [
     path('class/<int:pk>/', CreditDetailView.as_view(), name='credit-detail'),
     path('school/<int:pk>/', InstitutionDetailView.as_view(), name='institution-detail'),
-    path('classes', CreditIndexView.as_view()),
-    path('schools', InstitutionIndexView.as_view())
+    path('classes', CreditIndexView.as_view(), name='classes'),
+    path('schools', InstitutionIndexView.as_view(), name='schools'),
 ]
